ba193de7e425980562287048334f1c9fe698f48a
Arthur Atul <arthur.atul@ithands.biz>
2021-08-26 20:16:06 +0530

    serve react frontend from the server

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const api = require('./routes/V1/api');
 
@@ -11,7 +12,13 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use('/v1', api);
+
+app.get('/*', (req, res) => {
+    return res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 
 module.exports = app;
