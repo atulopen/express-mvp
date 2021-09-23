55466ba81c9a5abcf2559428a1ec3badf6182b3d
Arthur Atul <arthur.atul@ithands.biz>
2021-09-23 20:34:40 +0530

    added mongoose, connect application with mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,32 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
 const {loadHabitablePlanets} = require('./models/planets.model')
-const {load} = require("nodemon/lib/rules");
-const PORT = 8000;
-
+const PORT = 9000;
+const MONGO_URL = 'mongodb+srv://arthur_learning:dSb7b2ttopKXsLzs@nasacluster.otwsj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const server = http.createServer(app);
 
 const loadData = async () => {
     await loadHabitablePlanets();
 }
 
+mongoose.connection.on('open', () => {
+    console.log('Mongo DB Connection successful!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+});
+
+async function connectToMongo() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+}
+
 loadData();
+connectToMongo();
 
 server.listen(PORT, () => {
     console.log(`Listening at ${PORT}`);
